c345873c1036bedb4cd9a329a95d7788834a8ace
sharath kumar <24778029+sharathDHD@users.noreply.github.com>
2019-08-12 03:02:55 +0530

    Update DNATranscription.py

--- DNATranscription.py
+++ DNATranscription.py
@@ -1,6 +1,17 @@
+"dosent work with invalid chars"
+
+def _convert1(dna,config={"G":"C","C":"G","T":"A","A":"U"}):
+    return "".join([config[x]  if x in config else x for x in list(dna) ])
+
 def _convert(dna,config={"G":"C","C":"G","T":"A","A":"U"}):
-    return "".join([config[x]  if x in config else return "Invalid Input" for x in list(dna) ])
+    "sort them first then solve"
+    mod = list(sorted(set(dna)))
+    for x i mod:
+        if x not in config:
+                return "Invalid Input"
+    return "".join([config[x] for x in list(dna) ])        
     
 
 #print(_convert(input()))
+print(_convert("AGG"))
 
